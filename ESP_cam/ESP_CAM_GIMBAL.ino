b64d184d625978fa124cc84ce1cb0502dd213687
sitibudi <85599618+sitibudi@users.noreply.github.com>
2022-06-18 22:49:54 +0700

    update 8
    camera view with html page

--- ESP_cam/ESP_CAM_GIMBAL.ino
+++ ESP_cam/ESP_CAM_GIMBAL.ino
@@ -13,19 +13,30 @@
 //#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
 //#define CAMERA_MODEL_ESP_EYE // Has PSRAM
 #define CAMERA_MODEL_AI_THINKER // Has PSRAM
-// ** Espressif Internal Boards **
-//#define CAMERA_MODEL_ESP32_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S3_CAM_LCD
 
 #include "camera_pins.h"
+
 //================================
-const char* ssid = "Redmi Note 9"; //http://192.168.161.62
-                                    //http://192.168.219.62
-const char* password = "akusayangkamu";
+const char* ssid = "Bro-Bor";
+const char* password = "9434276267";
 //================================
-//const char* ssid = "Bro-Bor";
-//const char* password = "9434276267";
+
+WiFiServer server(80);
+bool connected = false;
+WiFiClient live_client;
+
+// setup html page 
+String index_html = "<meta charset=\"utf-8\"/>\n" \
+                    "<style>\n" \
+                    "#content {\n" \
+                    "display: flex;\n" \
+                    "flex-direction: column;\n" \
+                    "justify-content: center;\n" \
+                    "align-items: center;\n" \
+                    "text-align: center;\n" \
+                    "min-height: 100vh;}\n" \
+                    "</style>\n" \
+                    "<body bgcolor=\"#000000\"><div id=\"content\"><h2 style=\"color:#ffffff\"></h2><img src=\"video\"></div></body>";
 
 
 
@@ -38,13 +49,13 @@ int dly = 5000; // nilai delay yang digunakan untuk mengatur jeda putaran servo 
 #define echof 14
 #define trigf 2
 long durationf,distancef ;
-
+bool depan = false;
 
 // define Back Ultrasnoic
 #define echob 15
 #define trigb 13
 long durationb,distanceb ;
-
+bool belakang = false;
 
 //rotate
 String rot;
@@ -71,7 +82,69 @@ FirebaseConfig config;
 
 
 //===========================================================FUNCTION=====================================================
-void startCameraServer();
+
+//continue sending camera frame
+void liveCam(WiFiClient &client){
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+      Serial.println("Frame buffer could not be acquired");
+      return;
+  }
+  client.print("--frame\n");
+  client.print("Content-Type: image/jpeg\n\n");
+  client.flush();
+  client.write(fb->buf, fb->len);
+  client.flush();
+  client.print("\n");
+  esp_camera_fb_return(fb);
+}
+
+// setup http for camera web page
+void http_resp(){
+  WiFiClient client = server.available();                          
+  if (client.connected()) {    
+      String req = "";
+      while(client.available()){
+        req += (char)client.read();
+      }
+      Serial.println("request " + req);
+      int addr_start = req.indexOf("GET") + strlen("GET");
+      int addr_end = req.indexOf("HTTP", addr_start);
+      if (addr_start == -1 || addr_end == -1) {
+          Serial.println("Invalid request " + req);
+          return;
+      }
+      req = req.substring(addr_start, addr_end);
+      req.trim();
+      Serial.println("Request: " + req);
+      client.flush();
+ 
+      String s;
+      if (req == "/")
+      {
+          s = "HTTP/1.1 200 OK\n";
+          s += "Content-Type: text/html\n\n";
+          s += index_html;
+          s += "\n";
+          client.print(s);
+          client.stop();
+      }
+      else if (req == "/video")
+      {
+          live_client = client;
+          live_client.print("HTTP/1.1 200 OK\n");
+          live_client.print("Content-Type: multipart/x-mixed-replace; boundary=frame\n\n");
+          live_client.flush();
+          connected = true;
+      }
+      else
+      {
+          s = "HTTP/1.1 404 Not Found\n\n";
+          client.print(s);
+          client.stop();
+      }
+    }      
+}
 
 // function for ultrasonic setup pin mode
 void ultrasonicsetup(){   
@@ -93,7 +166,7 @@ void rotate(){
 
 // function to activate front ultrasonic 
 void frontsystem(){
-
+  depan = false;
   digitalWrite(trigf,LOW);
   delayMicroseconds(2);
   digitalWrite(trigf,HIGH);
@@ -104,22 +177,24 @@ void frontsystem(){
   distancef = durationf / 58.2; // formula to convert ulrasonic pulse to distance value
 //  Serial.println(distancef); // to print distance value
 
-  if(distancef <jarakf){  //jika mendeteksi jarak yang ditentukan maka arduino mengirim angka 1 yang ke esp yang artinya ada benda/orang yang terdeteksi di mobil
-  int tiga = 1; 
+  if(distancef <jarakf){  //jika mendeteksi jarak yang ditentukan maka arduino mengirim 
+  depan = false;
+  int nilai = 1; // angka 3 yang ke esp yang artinya ada benda/orang yang terdeteksi di mobil
   //Firebase.setInt(dataObject,"path",target)
-  Firebase.setInt(fbdo,"/ESP_Cam/alarm",tiga);  // send data to firebase 
+  Firebase.setInt(fbdo,"/ESP_Cam/alarm",nilai);  // send data to firebase 
 
   }
   
   else{ //jika tidak maka arduino mengirim angka 0 yang artinya tidak ada benda yang terdeteksi
-  int dua = 0;
-  Firebase.setInt(fbdo,"/ESP_Cam/alarm",dua);
+  int nilai = 0;
+  Firebase.setInt(fbdo,"/ESP_Cam/alarm",nilai);
+  depan = true;
   }
 }
 
 //function to activate back ultrasonic 
 void backsystem(){
- 
+  belakang = false;
    digitalWrite(trigb,LOW);
   delayMicroseconds(2);
   digitalWrite(trigb,HIGH);
@@ -128,19 +203,17 @@ void backsystem(){
   durationb = pulseIn(echob,HIGH);
   
   distanceb = durationb / 58.2;
-//   Serial.println(distanceb);
+  Serial.println(distanceb);
   if(distanceb < jarakb){
-    
-  int tiga = 1;
-  Firebase.setInt(fbdo,"/ESP_Cam/alarm",tiga);
-//     servo0.write(180); // servo memutar 180 derajat 
-//     delay(dly); // delay digunakan untuk menahan servo disudut 180 derajat dengan jeda waktu yang ditentukan
+    belakang = false;
+  int nilai = 1;
+  Firebase.setInt(fbdo,"/ESP_Cam/alarm",nilai);
   }
   else{
-   
-    int dua = 0;
-    Firebase.setInt(fbdo,"/ESP_Cam/alarm",dua);
-//     servo0.write(0);  //servo kembali ke posisi awal (0) jika tidak sensor tidak mendeteksi apa-apa
+    belakang = true;
+    int nilai = 0;
+    Firebase.setInt(fbdo,"/ESP_Cam/alarm",nilai);
+
   }
 
   
@@ -171,7 +244,7 @@ void espsetup(){
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.frame_size = FRAMESIZE_UXGA;
+  config.frame_size = FRAMESIZE_VGA;
   config.pixel_format = PIXFORMAT_JPEG; // for streaming
   //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
   config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
@@ -179,31 +252,6 @@ void espsetup(){
   config.jpeg_quality = 12;
   config.fb_count = 1;
   
-  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-  //                      for larger pre-allocated frame buffer.
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    if(psramFound()){
-      config.jpeg_quality = 10;
-      config.fb_count = 2;
-      config.grab_mode = CAMERA_GRAB_LATEST;
-    } else {
-      // Limit the frame size when PSRAM is not available
-      config.frame_size = FRAMESIZE_SVGA;
-      config.fb_location = CAMERA_FB_IN_DRAM;
-    }
-  } else {
-    // Best option for face detection/recognition
-    config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-    config.fb_count = 2;
-#endif
-  }
-
-#if defined(CAMERA_MODEL_ESP_EYE)
-  pinMode(13, INPUT_PULLUP);
-  pinMode(14, INPUT_PULLUP);
-#endif
-
   // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
@@ -211,26 +259,10 @@ void espsetup(){
     return;
   }
 
+  // settings camera
   sensor_t * s = esp_camera_sensor_get();
-  // initial sensors are flipped vertically and colors are a bit saturated
-  if (s->id.PID == OV3660_PID) {
-    s->set_vflip(s, 1); // flip it back
-    s->set_brightness(s, 1); // up the brightness just a bit
-    s->set_saturation(s, -2); // lower the saturation
-  }
-  // drop down frame size for higher initial frame rate
-  if(config.pixel_format == PIXFORMAT_JPEG){
-    s->set_framesize(s, FRAMESIZE_QVGA);
-  }
-
-#if defined(CAMERA_MODEL_M5STACK_WIDE) || defined(CAMERA_MODEL_M5STACK_ESP32CAM)
-  s->set_vflip(s, 1);
-  s->set_hmirror(s, 1);
-#endif
-
-#if defined(CAMERA_MODEL_ESP32S3_EYE)
-  s->set_vflip(s, 1);
-#endif
+  s->set_vflip(s, 1);          // 0 = disable , 1 = enable vertical flip
+  
 //===========================WIFI ACCESS POINT============================================
   WiFi.begin(ssid, password);
   WiFi.setSleep(false);
@@ -241,17 +273,14 @@ void espsetup(){
   }
   Serial.println("");
   Serial.println("WiFi connected");
-//===========================ESP ACCESS POINT============================================
-//  WiFi.softAP(ssid,password);
-//  IPAddress IP = WiFi.softAPIP();
-//  Serial.print("AP IP address:");
-//  Serial.println(IP);
-//==============================DONE======================================================
-  startCameraServer();
-
   Serial.print("Camera Ready! Use 'http://");
-  Serial.print(WiFi.localIP());
-  Serial.println("' to connect");
+  String IP = WiFi.localIP().toString();
+  Serial.print(IP);
+  Serial.println("' to connect"+IP);
+  index_html.replace("server_ip",IP );
+  server.begin();
+ 
+ //==============================DONE======================================================
 }
 
 // function for setup and connect firebase 
@@ -291,6 +320,12 @@ void setup() {
 }
 
 void loop() {
+  // running live camera 
+    http_resp();
+  if(connected == true){
+    liveCam(live_client);
+  }
+  
 //  =========================SECURITY LOGIC=====================================
   Firebase.getString(fbdo,"/ESP_Cam/security",security); // firebase get the string from firebase from a spesific path
  security = fbdo.stringData(); // move string data from firebase reading to the provided variable
@@ -298,11 +333,12 @@ void loop() {
   if(security == "1"){ // jika security == 1 atau keamanan diaktifkan maka akan memanggil function untuk mengaktifkan sistem keamanan depan dan belakang
   backsystem(); 
   frontsystem();
-  rotate();  
+  rotate();
+  
   }
   else{
-  int dua = 0;
-  Firebase.setInt(fbdo,"/ESP_Cam/alarm",dua);
+  int nilai = 0;
+  Firebase.setInt(fbdo,"/ESP_Cam/alarm",nilai);
 
   rot = "0";
   Firebase.setString(fbdo,"/ESP_Cam/rotate",rot);
